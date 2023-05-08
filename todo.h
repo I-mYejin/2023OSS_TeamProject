95a54e1fc9c36ae0c83d356159e7bddb9c7fcbed
EundaChung <22200680@handong.ac.kr>
2023-05-08 20:04:53 +0900

    add 'addTodo() function'

--- todo.h
+++ todo.h
@@ -6,9 +6,11 @@ typedef struct {
     char deadline[20]; //ex) 2023-05-08
     char day[10]; // ex) Mon
     int hrs; //ex) 4(예상 소요 시간)
-    int check; // ex) 0, 1(한 일 입력받아서 √표시)
+    int check=0; // ex) 0, 1(한 일 입력받아서 √표시)
 } ToDo;
 
 int deleteTodo(ToDo *s); //일정 삭제
 void todoCheck(ToDo s); //일정 완료 체크 표시
 void todoRead(ToDo s); //일정 리스트 출력
+
+int addTodo(ToDo *t); //일정 추가
