95351769dd6799d647ee83c9314cbb068302f02e
I-mYejin <127292731+I-mYejin@users.noreply.github.com>
2023-05-09 22:08:32 +0900

    Update todo.h

--- todo.h
+++ todo.h
@@ -6,12 +6,13 @@ typedef struct {
     char deadline[20]; //ex) 2023-05-08
     char day[10]; // ex) Mon
     int hrs; //ex) 4(예상 소요 시간)
-    int check=0; // ex) 0, 1(완료하면 1 입력받아서 √표시)
+    int check = 0; // ex) 0, 1(완료하면 1 입력받아서 √표시)
 } ToDo;
 
-int deleteTodo(ToDo *s); //일정 삭제
-void todoCheck(ToDo s); //일정 완료 체크 표시
-void todoRead(ToDo s); //일정 리스트 출력
+int deleteTodo(ToDo *t); //일정 삭제
+void todoRead(ToDo t); //일정 리스트 출력
+int listTodo(ToDo *t[], int count);
 
 int addTodo(ToDo *t); //일정 추가
 int updateTodo(ToDo *t); //일정 수정: return addTodo()
+
