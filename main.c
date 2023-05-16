71c7fc63be2d84ada345eda63e313c6d9eceefc6
I-mYejin <127292731+I-mYejin@users.noreply.github.com>
2023-05-16 20:38:57 +0900

    Update main.c

--- main.c
+++ main.c
@@ -11,28 +11,28 @@ int main(void) {
     menu = selectMenu();
     if(menu == 0) break;
     
-    if (menu == 1) {
+    if (menu == 1) { //일정 조회
       if(count > 0) {
         listTodo(t, index);
       }
     }
     
-    else if(menu == 2){
+    else if(menu == 2){ //일정 추가
       count += 1;
       addTodo(&t[index++]);
 
     }
-    else if (menu == 3){
+    else if (menu == 3){ //일정 수정
       updateTodo(t);
 
     }
-    else if (menu == 4){
+    else if (menu == 4){ //일정 삭제
 
     }
-    else if (menu == 5){
+    else if (menu == 5){ //파일 저장
 
     }
-    else if(menu == 6){
+    else if(menu == 6){ //파일 불러오기
         
     }
 
